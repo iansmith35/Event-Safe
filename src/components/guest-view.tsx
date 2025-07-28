58fdfd746460812b977f250494117a4a17a77bc8
Ian Smith <ian@ishe-ltd.co.uk>
2025-07-28 20:19:55 +0000

    Error: ./src/components/guest-view.tsx:77:6 Parsing ecmascript source co

--- src/components/guest-view.tsx
+++ src/components/guest-view.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
@@ -311,3 +312,5 @@ export default function GuestView() {
     </div>
   );
 }
+
+    
